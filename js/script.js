a1ec2b3c6402a8c4bed111db153cc0f5eb029d8b
Beboto <97461438+Beboto@users.noreply.github.com>
2022-10-15 00:39:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 }
 
+function formValidate(){
+  const fname=document.getElementById('fname').value;
+  const inEmail=document.getElementById('inEmail').value;
+  const phone=document.getElementById('phone').value;
+
+
+  function validateEmail(email){
+    const pattern=/\S+@\S+\.\S+/;
+    return pattern.test(email);
+  }
+
+  function validatePhone(phoneNumber){
+    const pattern=/^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+    return pattern.test(phoneNumber);
+  }
+
+  if(fname===""){
+    alert("Enter your name.");
+  }else if(!validatePhone(phone)){
+      alert("Enter your phone number.")
+    }else if(!validateEmail(inEmail)){
+      alert("Enter a valid email id example : 'mymail@gmail.com'")
+  }
+};
+
